77f816ddf1056f6d3e830452ba9b488fa770a96d
Yuiko Koyanagi <yuiko@YuikonoMBP.fritz.box>
2024-07-03 16:04:32 +0200

    chore:tada: add submit button

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HousingLocation } from '../housing-location/housing-location';
 import { HousingLocationService } from '../housing-location/housing-location.service';
@@ -6,7 +8,7 @@ import { HousingLocationService } from '../housing-location/housing-location.ser
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
@@ -14,9 +16,21 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   HousingLocationService = inject(HousingLocationService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation =
       this.HousingLocationService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.HousingLocationService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
